80b99f04c1ed5893edd7d8fe816719f0f444a276
Eam145vc <emalvarez930120@gmail.com>
2025-04-01 13:34:09 -0500

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -42,16 +42,3 @@ const admin = (req, res, next) => {
 };
 
 module.exports = { protect, admin };
-
-// middleware/validate.js
-const { validationResult } = require('express-validator');
-
-const validateRequest = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
-};
-
-module.exports = { validateRequest };
